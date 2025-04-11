ec0c771727f0d6f917f5c7c540fd024f516bafaf
Kim1ni <mathengegabriel9@gmail.com>
2025-04-11 17:11:12 +0300

    Modified TodoRepositoryTest.kt

--- app/src/test/java/com/example/todoapp/repository/TodoRepositoryTest.kt
+++ app/src/test/java/com/example/todoapp/repository/TodoRepositoryTest.kt
@@ -6,7 +6,6 @@ import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mockito.any
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 import org.mockito.Mockito.`when`
@@ -24,58 +23,64 @@ class TodoRepositoryTest {
     }
 
     @Test
-    fun insertTodo_callsDao(){
+    fun insertTodo_callsDao() {
         runBlocking {
             // Given
             val title = "Test Todo"
             val description = "Test Description"
-            `when`(todoDao.insertTodo(any())).thenReturn(1L)
+            `when`(todoDao.insertTodo(Todo(title = title, description = description))).thenReturn(1L)
 
             // When
             val result = repository.insertTodo(title, description)
 
             // Then
             assertEquals(1L, result)
-            verify(todoDao).insertTodo(any())
+            verify(todoDao).insertTodo(Todo(title = title, description = description))
         }
     }
 
     @Test
-    fun toggleTodoCompleted_updatesCompletionStatus() = runBlocking {
-        // Given
-        val date = Date()
-        val todo = Todo(id = 1, title = "Test", createdDate = date)
+    fun toggleTodoCompleted_updatesCompletionStatus() {
+        runBlocking {
+            // Given
+            val date = Date()
+            val todo = Todo(id = 1, title = "Test", createdDate = date)
 
-        // When
-        repository.toggleTodoCompleted(todo)
+            // When
+            repository.toggleTodoCompleted(todo)
 
-        // Then
-        verify(todoDao).updateTodo(argThat {
-            this.id == 1L && this.isCompleted && this.completedDate != null
-        })
+            // Then
+            verify(todoDao).updateTodo(argThat { updatedTodo ->
+                updatedTodo.id == 1L && updatedTodo.isCompleted && updatedTodo.completedDate != null
+            })
+        }
     }
 
     @Test
-    fun toggleTodoCompleted_undoesCompletionStatus() = runBlocking {
-        // Given
-        val date = Date()
-        val todo = Todo(id = 1, title = "Test", createdDate = date, isCompleted = true, completedDate = date)
+    fun toggleTodoCompleted_undoesCompletionStatus() {
+        runBlocking {
+            // Given
+            val date = Date()
+            val todo = Todo(id = 1, title = "Test", createdDate = date, isCompleted = true, completedDate = date)
 
-        // When
-        repository.toggleTodoCompleted(todo)
+            // When
+            repository.toggleTodoCompleted(todo)
 
-        // Then
-        verify(todoDao).updateTodo(argThat {
-            this.id == 1L && !this.isCompleted && this.completedDate == null
-        })
+            // Then
+            verify(todoDao).updateTodo(argThat { updatedTodo ->
+                updatedTodo.id == 1L && !updatedTodo.isCompleted && updatedTodo.completedDate == null
+            })
+        }
     }
 
     @Test
-    fun deleteAllCompletedTodos_callsDao() = runBlocking {
-        // When
-        repository.deleteAllCompletedTodos()
+    fun deleteAllCompletedTodos_callsDao() {
+        runBlocking {
+            // When
+            repository.deleteAllCompletedTodos()
 
-        // Then
-        verify(todoDao).deleteAllCompletedTodos()
+            // Then
+            verify(todoDao).deleteAllCompletedTodos()
+        }
     }
 }
